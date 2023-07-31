471bfec520352ce9f897d9a866289ef394fdc352
Sarah Squyres <ssquyres15@gmail.com>
2023-07-31 18:54:24 -0500

    pushing prior to mvp

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const listRoutes = require('./listRoutes');
 const taskRoutes = require('./taskRoutes');
-const listRoutes = require('./listRoutes');
 
 router.use('/users', userRoutes);
 router.use('/lists', listRoutes);
 router.use('/tasks', taskRoutes);
-router.use('/lists', listRoutes);
 
 module.exports = router;
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -57,26 +57,30 @@ console.log(userData);
   }
 });
 
-//update user info- username, password, first name, last name, bio, and profile picture
-router.put('/:id', async (req, res) => {
+//find a user by id and add first name, last name, bio, and profile picture
+router.put("/:id", async (req, res) => {
   try {
-    const userData = await User.update(req.body, {
-      where: {
+    const userData = await User.update(
+      {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         bio: req.body.bio,
         profile_picture: req.body.profile_picture,
       },
-    });
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
     if (!userData) {
-      res.status(404).json({ message: 'No user found with this id!' });
+      res.status(404).json({ message: "No user found with this id!" });
       return;
     }
     res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 //delete user
