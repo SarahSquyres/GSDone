c1717ed0a55dfe42fe61ff825124af0a4d6ba7ca
Freebird <bo.salinas12@gmail.com>
2023-07-30 19:05:22 -0500

    started feedpage html structure

--- models/User.js
+++ models/User.js
@@ -20,20 +20,24 @@ class User extends Model {
         type: DataTypes.STRING,
         allowNull: false,
       },
-    //   email: {
-    //     type: DataTypes.STRING,
-    //     allowNull: false,
-    //     unique: true,
-    //     validate: {
-    //       isEmail: true,
-    //     },
-    //   },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
           len: [8],
         },
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
       },
     },
     {
