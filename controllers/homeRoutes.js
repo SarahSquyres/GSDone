99ace6154b263e999f85036e5f1625c8fa03f405
Freebird <bo.salinas12@gmail.com>
2023-07-31 21:40:36 -0500

    login.js and working on profile/list page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,19 +2,56 @@
 const router = require('express').Router();
 const { User } = require('../models');
 // const withAuth = require('../utils/auth');
-//if we add a route to user profile uncommecment withAuth
+
+
+//route to homepage
 router.get('/', async (req, res) => {
   const userData = await User.findAll()
 
   const users = userData.map((user) =>
-  user.get({ plain: true })
+    user.get({ plain: true })
   );
-
-
-  // Send the rendered Handlebars.js template back as the response
   res.render('homepage', {
-    users, 
+    users,
   });
 });
 
+//route to profile page using auth 
+router.get('/profile', async (req, res) => {
+  try {
+   
+    const userData = await User.findByPk(req.session.id, {
+      attributes: { exclude: ['password'] }
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//route to login page 
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+//route to feeds page
+router.get('/feedpage', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/login');
+    return;
+  }
+
+  res.render('feedpage');
+});
 module.exports = router;
