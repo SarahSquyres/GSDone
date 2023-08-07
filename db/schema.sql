90b5148b79c9d43427d90d5d8032d84cf9ba0d42
Sarah Squyres <126419905+SarahSquyres@users.noreply.github.com>
2023-08-07 18:11:43 -0500

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,12 @@ CREATE DATABASE user_db;
 
 USE user_db;
 
-CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE user (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) NOT NULL,
+--     password VARCHAR(100) NOT NULL,
+--     PRIMARY KEY (id)
+-- );
 
-USE user_db;
+-- USE user_db;
